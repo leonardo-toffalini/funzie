34c9a294f7bcfca6d3c9c430e466a231c955d7b6
leonardo-toffalini <leonardotoffalini@gmail.com>
2024-12-21 20:30:49 +0100

    platonic solids in lua?

--- main.lua
+++ main.lua
@@ -7,19 +7,32 @@ local alpha = math.pi / 16
 local beta = math.pi / 32
 local gamma = math.pi / 64
 
-local shape = shapes.Icosahedron()
+local shape = nil
 
 function love.load(args)
-  for i = 1, #args do
-    print(args[i])
+  shapeInput = args[1]
+  if shapeInput == "cube" or shapeInput == "6" then shape = shapes.Cube()
+  elseif shapeInput == "tetrahedron" or shapeInput == "4" then shape = shapes.Tetrahedron()
+  elseif shapeInput == "octahedron" or shapeInput == "8" then shape = shapes.Octahedron()
+  elseif shapeInput == "icosahedron" or shapeInput == "20" then shape = shapes.Icosahedron()
+  elseif shapeInput == "dodecahedron" or shapeInput == "12" then shape = shapes.Dodecahedron()
+  else
+    print("Not recognized shape input: " .. shapeInput)
+    print("Available shapes:\n\tcube (6), tetrahedron (4), octahedron (8), icosahedron (20), dodecahedron (12)")
+    os.exit()
   end
+
 end
 
 function love.update(dt)
-  shape:rotate(alpha, beta, gamma, dt)
+  if shape ~= nil then
+    shape:rotate(alpha, beta, gamma, dt)
+  end
 end
 
 function love.draw()
   love.graphics.translate(width / 2, height / 2)
-  shape:draw("all", fov, scale)
+  if shape ~= nil then
+    shape:draw("all", fov, scale)
+  end
 end
